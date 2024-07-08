27c794c8a5f8ca83420d952520dfd03743e59ca5
shigatsi <shigatsii@gmail.com>
2024-07-08 23:00:57 +0300

    refactor: ui component created, add thems'

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from "react";
+import { classNames } from "shared/helpers";
+import cls from "./AppLink.module.scss";
+import { LinkProps } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+export enum AppLinkTheme {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+}
+
+interface IAppLink extends LinkProps {
+  className?: string;
+  theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<IAppLink> = ({
+  to,
+  children,
+  className,
+  theme = AppLinkTheme.PRIMARY,
+  ...otherProps
+}) => {
+  return (
+    <Link
+      to={to}
+      className={classNames(cls.AppLink, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import { classNames } from "shared/helpers";
 import cls from "./Navbar.module.scss";
+import { AppLink, AppLinkTheme } from "shared/ui";
 
 interface INavbar {
   className?: string;
@@ -10,8 +9,14 @@ interface INavbar {
 export const Navbar = ({ className }: INavbar) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
-      <Link to={"/"}>На главную</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <div className={cls.links}>
+        <AppLink className={cls.link} to={"/"} >
+          На главную
+        </AppLink>
+        <AppLink className={cls.link} to={"/about"} theme={AppLinkTheme.SECONDARY}>
+          О сайте
+        </AppLink>
+      </div>
     </div>
   );
 };
