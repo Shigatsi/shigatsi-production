8d80b594afd224624a9330237bff4b1982f5fa41
shigatsi <shigatsii@gmail.com>
2024-07-08 23:32:45 +0300

    refactor

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,6 @@
 import { classNames } from "shared/helpers";
 import cls from "./Navbar.module.scss";
-import { AppLink, AppLinkTheme } from "shared/ui";
+import { AppLink, AppLinkTheme, AppSwitcher } from "shared/ui";
 
 interface INavbar {
   className?: string;
@@ -10,13 +10,18 @@ export const Navbar = ({ className }: INavbar) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
       <div className={cls.links}>
-        <AppLink className={cls.link} to={"/"} >
+        <AppLink className={cls.link} to={"/"}>
           На главную
         </AppLink>
-        <AppLink className={cls.link} to={"/about"} theme={AppLinkTheme.SECONDARY}>
+        <AppLink
+          className={cls.link}
+          to={"/about"}
+          theme={AppLinkTheme.SECONDARY}
+        >
           О сайте
         </AppLink>
       </div>
+      <AppSwitcher />
     </div>
   );
 };
