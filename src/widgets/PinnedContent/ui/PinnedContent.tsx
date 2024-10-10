2d9899be7d3b53b076b2d45d72e80f4c924a67d8
shigatsi <shigatsii@gmail.com>
2024-10-10 18:26:42 +0300

    feat: widget for pinned content added

---
+++ src/widgets/PinnedContent/ui/PinnedContent.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { classNames } from "shared/helpers";
+import cls from "./PinnedContent.module.scss";
+import { AppButton } from "shared/ui";
+
+interface IPinnedContent {
+    title: string;
+    data: any[];
+    className?: string;
+}
+
+export const PinnedContent = ({ title, data, className }: IPinnedContent) => {
+    return (
+        <section className={classNames(cls.pinned, {}, [className])}>
+            <div className={classNames(cls.pinned__header, {}, [className])}>
+                <h2 className={classNames(cls.pinned__title, {}, [className])}>
+                    {title}
+                </h2>
+                <AppButton>add new {title}</AppButton>
+                <AppButton>go to all {title}</AppButton>
+            </div>
+            <div className={classNames(cls.pinned__content, {}, [className])}>
+                {data.map((el, i) => (
+                    <div
+                        key={i}
+                        className={classNames(cls.pinned__item, {}, [
+                            className,
+                        ])}
+                    >
+                        {el}
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,1 +1,2 @@
 export { Navbar } from "./Navbar/ui/Navbar";
+export { PinnedContent } from "./PinnedContent/ui/PinnedContent";
